ff8626f4f35a5e43418a1be36a2995532b4ae7d7
Rabvc4 <chill_zero@hotmail.com>
2018-04-12 16:12:46 -0500

    Great progress made across the board, too much to mention

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
@@ -13,12 +14,12 @@ app.secret_key = 'y337kGcys&zP3B'
 class Review(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120))
-    content = db.Column(db.String(1000))
-    review = db.Column(db.String(120))
-    food = db.Column(db.String(120))
-    rating = db.Column(db.Integer)
-    date = db.Column(DateTime, default=datetime.datetime.utcnow)
+    title = db.Column(db.String(120), nullable=False)
+    content = db.Column(db.String(1000), nullable=False)
+    review = db.Column(db.String(120), nullable=False)
+    food = db.Column(db.String(120), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.Date, nullable=False)
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'))
 
     def __init__(self, title, content, review, food, rating, date, restaurant):
@@ -34,7 +35,7 @@ class Review(db.Model):
 class Restaurant(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
+    name = db.Column(db.String(120), nullable=False)
     image = db.Column(db.String(2048))
     reviews = db.relationship('Review', backref='restaurant')
 
@@ -46,16 +47,41 @@ class Restaurant(db.Model):
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
-    if request.method == 'POST':
-        task_name = request.form['review']
-        new_task = Task(task_name, owner)
-        db.session.add(new_task)
-        db.session.commit()
+    return render_template('blog.html', )
 
-    tasks = Task.query.filter_by(completed=False,owner=owner).all()
-    completed_tasks = Task.query.filter_by(completed=True,owner=owner).all()
-    return render_template('todos.html',title="Get It Done!",
-        tasks=tasks, completed_tasks=completed_tasks)
+@app.route('/newpost', methods=['POST', 'GET'])
+def newpost():
+    if request.method == 'POST':
+        name = request.form['name']
+        image = request.form['image']
+        title = request.form['title']
+        content = request.form['content']
+        review = request.form['review']
+        food = request.form['food']
+        rating = request.form['rating']
+        submitted_date = request.form['date']
+        date = datetime.strptime(date, '%Y %b %d')
+
+        # TODO - validate user's data
+
+        existing_restaurant = Restaurant.query.filter_by(name=name).first()
+        if not existing_restaurant:
+            new_restaurant = Restaurant(name, image)
+            db.session.add(new_restaurant)
+            db.session.commit()
+            new_restaurant = Restaurant.query.filter_by(name=name).first()
+            new_review = Review(title, content, review, food, rating, date, new_restaurant.id)
+            db.session.add(new_review)
+            db.session.commit()
+            return redirect('/')
+        else:
+            flash('Restaurant already exists', 'error')
+            new_review = Review(title, content, review, food, rating, date, existing_restaurant.id)
+            db.session.add(new_review)
+            db.session.commit()
+
+# TODO make correct fields repopulate
+    return render_template('newpost.html')
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0')
