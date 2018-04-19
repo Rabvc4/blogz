b07cc64666683ddbd22e7a791960fa9365c58107
Rabvc4 <chill_zero@hotmail.com>
2018-04-19 15:35:53 -0500

    Initial Commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, request, redirect, render_template, session, flash
+from flask import Flask, request, redirect, render_template, session, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 import os
@@ -10,46 +10,49 @@ UPLOAD_FOLDER = './static/images'
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:launchcode@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:MyNewPass@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-app.secret_key = 'y337kGcys&zP3B'
+app.secret_key = 'dogs'
 
-class Review(db.Model):
+class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False)
     content = db.Column(db.String(1000), nullable=False)
-    review = db.Column(db.String(120), nullable=False)
-    food = db.Column(db.String(120), nullable=False)
-    rating = db.Column(db.Integer, nullable=False)
     date = db.Column(db.Date, nullable=False)
-    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, content, review, food, rating, date, restaurant):
+    def __init__(self, title, content, date, owner):
         self.title = title
         self.content = content
-        self.review = review
-        self.food = food
-        self.rating = rating
         self.date = date
-        self.restaurant_id = restaurant
+        self.owner = owner
 
     def __repr__(self):
-        return '<Review %r>' % self.title
+        return '<Blog %r>' % self.title
 
-class Restaurant(db.Model):
+class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), nullable=False)
-    image = db.Column(db.String(2048))
-    reviews = db.relationship('Review', backref='restaurant')
+    username = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(120), nullable=False)
+    first_name = db.Column(db.String(120), nullable=False)
+    last_name = db.Column(db.String(120), nullable=False)
+    avatar = db.Column(db.String(2048), default="avatar.png")
+    blog = db.relationship('Blog', backref='owner')
 
-    def __init__(self, name, image):
-        self.name = name
-        self.image = image
+    def __init__(self, username, password, first_name, last_name, avatar, blog):
+        self.username = username
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
+        self.avatar = avatar
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -69,6 +72,48 @@ def today():
     date = now.strftime("%m-%d-%Y")
     return str(date)
 
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'signup']
+    if request.endpoint not in allowed_routes and 'username' not in session:
+        return redirect('/login')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            session['username'] = username
+            flash("Logged in")
+            return redirect('/')
+        else:
+            flash('User password incorrect or user does not exist', 'error')
+
+    return render_template('login.html')
+
+@app.route('/signup', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        # TODO - validate user's password
+
+        existing_user = User.query.filter_by(username=username).first()
+        if not existing_user:
+            new_user = User(username, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = username
+            return redirect('/')
+        else:
+            flash('User already exists', 'error')
+
+    return render_template('signup.html')
+
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
     date = str(today())
@@ -76,9 +121,6 @@ def newpost():
         name = request.form['name']
         title = request.form['title']
         content = request.form['content']
-        review = request.form['review']
-        food = request.form['food']
-        rating = request.form['rating']
         date = request.form['date']
         if 'file' not in request.files:
             flash('No file found', 'error')
@@ -115,27 +157,13 @@ def newpost():
             db.session.add(new_review)
             db.session.commit()
             return redirect('/blog')
-        else:
-            flash('Restaurant already exists')
-            new_review = Review(title, content, review, food, rating, sql_date_format(date), existing_restaurant.id)
-            db.session.add(new_review)
-            db.session.commit()
-            return redirect('/blog')
-
 
     return render_template('newpost.html', date=date)
 
-@app.route('/blog', methods=['POST', 'GET'])
+app.route('/', methods=['POST', 'GET'])
 def index():
-    if request.method == 'GET':
-        if 'id' in request.args:
-            review_id = int(request.args.get('id'))
-            print("FIRST REVIEW ID: ",review_id)
-            review = Review.query.join(Restaurant).add_columns(Restaurant.name, Restaurant.image, Review.title, Review.content, Review.food, Review.review, Review.rating, Review.date).filter(Review.id==review_id).first()
-            return render_template('review.html', review=review)
-        else:
-            blog = Review.query.join(Restaurant).add_columns(Review.id, Restaurant.name, Restaurant.image, Review.title, Review.content, Review.food, Review.review, Review.rating, Review.date).order_by(Review.date.desc()).all()
-            return render_template('blog.html', blog=blog)
+
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0')
